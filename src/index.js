d0e6f94d867d62fe1e2ea90c76f40dc0b7f15d3f
Tatsuo Nomura <tatsuo@nianticlabs.com>
2020-09-04 00:01:22 +0900

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) Tatsuo Nomura <tatsuo.nomura@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict'
+
+console.log("Hello World!!");
