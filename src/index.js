eaec651239075626225d8b12abe33393897d5ddc
Tatsuo Nomura <tatsuo.nomura@gmail.com>
2020-09-04 00:53:03 +0900

    Add a server that logs the incoming messages.

--- src/index.js
+++ src/index.js
@@ -7,4 +7,20 @@
 
 'use strict'
 
-console.log("Hello World!!");
+import { createServer } from "net";
+
+const log = console.log;
+const server = createServer(socket => {
+  log(`Connection accepted: ${socket.remoteAddress}:${socket.remotePort}`)
+  socket.on("data", (data) => {
+    log(`Incoming data:${data}`);
+  });
+  socket.on("close", () => log("Connection closed"));
+});
+server.on("error", err => { throw err; });
+server.on("close", () => log("Echo Server, Shutdown"));
+
+const port = 2424;
+server.listen({ host: "localhost", port: port }, () => {
+  log(`Started a server at ${port}`)
+});
