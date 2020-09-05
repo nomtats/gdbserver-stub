6edfff0c46f017fb1091eac061110e7cd7df638a
Tatsuo Nomura <tatsuo.nomura@gmail.com>
2020-09-05 23:35:23 +0900

    Extract packet data from the incoming data.

--- src/index.js
+++ src/index.js
@@ -10,13 +10,37 @@
 import { createServer } from "net";
 
 const log = console.log;
+const error = console.error;
+
 const server = createServer(socket => {
   log(`Connection accepted: ${socket.remoteAddress}:${socket.remotePort}`)
   socket.on("data", (data) => {
-    log(`<-:${data}`);
-    const reply = "+$#00";
-    log(`->:${reply}`)
-    socket.write(reply);
+    let input = data.toString();
+    while (input.length > 0) {
+      let m;
+      if (m = input.match(/^\+/)) {
+        // ack
+        log(`<-:${m[0]}`);
+      } else if (m = input.match(/^\$([^#]*)#([0-9a-zA-Z]{2})/)) {
+        log(`<-:${m[0]}`);
+        const packet = m[1];
+        const checksum = parseInt(m[2], 16);
+        const expected = computeChecksum(packet);
+        if (checksum == expected) {
+          const reply = '+$#00';
+          log(`->:${reply}`);
+          socket.write(reply);
+        } else {
+          error(`Invalid checksum. Expected ${expected.toString(16)} but received ${m[2]} for packet ${packet}`);
+        }
+      } else {
+        log(`<-:${input}`);
+        error(`Unkown incoming message: ${input}`);
+        // Ignore the rest of the data.
+        break;
+      }
+      input = input.substr(m[0].length);
+    }
   });
   socket.on("close", () => log("Connection closed"));
 });
@@ -27,3 +51,9 @@ const port = 2424;
 server.listen({ host: "localhost", port: port }, () => {
   log(`Started a server at ${port}`)
 });
+
+function computeChecksum(packet) {
+  return packet.split('')
+    .map(x => x.charCodeAt(0))
+    .reduce((a, b) => (a + b) & 0xff);
+}
